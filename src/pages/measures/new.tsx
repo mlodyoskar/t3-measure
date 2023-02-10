6b630704d8b59e05c89754e622287332284f3310
Oskar Puchalski <oskarpuchalski17@gmail.com>
2023-02-10 15:12:10 +0100

    feat: user can add new measurement from form

--- src/pages/measures/new.tsx
+++ src/pages/measures/new.tsx
@@ -3,11 +3,12 @@ import Form from "../../components/forms/Form";
 import { useZodForm } from "../../utils/useZodForm";
 import { Input } from "../../components/forms/Input";
 import { Button } from "../../components/ui/Button";
+import { api } from "../../utils/api";
 
 const positiveDecimalRegex = /^\d+(\.\d{1})?$/;
 const MEASURE_FIELD_ERROR_MESSAGE = "Wartość musi być liczbą dodatnią";
 
-const formFieldNames = [
+export const formFieldNames = [
   {
     name: "calf",
     displayName: "Łydka",
@@ -21,13 +22,12 @@ const formFieldNames = [
     displayName: "Kark",
   },
   {
-    name: "neck",
-    displayName: "Kark",
-  },
-
-  {
     name: "chest",
     displayName: "Klatka piersiowa",
+  },
+  {
+    name: "waist",
+    displayName: "Pas",
   },
   {
     name: "biceps",
@@ -35,19 +35,22 @@ const formFieldNames = [
   },
 ] as const;
 
-const CreateMeasure = z.object({
+export type FormFieldNames = (typeof formFieldNames)[number]["name"];
+
+export const CreateMeasure = z.object({
   date: z.date(),
   weight: z.string(),
   neck: z.string().regex(positiveDecimalRegex, MEASURE_FIELD_ERROR_MESSAGE),
   tigh: z.string().regex(positiveDecimalRegex, MEASURE_FIELD_ERROR_MESSAGE),
   calf: z.string().regex(positiveDecimalRegex, MEASURE_FIELD_ERROR_MESSAGE),
+  waist: z.string().regex(positiveDecimalRegex, MEASURE_FIELD_ERROR_MESSAGE),
   chest: z.string().regex(positiveDecimalRegex, MEASURE_FIELD_ERROR_MESSAGE),
   biceps: z.string().regex(positiveDecimalRegex, MEASURE_FIELD_ERROR_MESSAGE),
 });
 
 const NewMeasurePage = () => {
   const form = useZodForm({ schema: CreateMeasure });
-  // const x = api.measure.create.useMutation({});
+  const { mutate } = api.measure.create.useMutation();
   const todaysDate = new Date().toISOString().slice(0, 10);
 
   return (
@@ -55,8 +58,9 @@ const NewMeasurePage = () => {
       <Form
         form={form}
         onSubmit={(data) => {
-          console.log(data);
-          // x.mutate({ text: "ASD" });
+          console.log(data.date.toISOString());
+          const res = mutate(data);
+          console.log(res);
         }}
       >
         <div className="flex flex-col gap-4">
@@ -77,7 +81,7 @@ const NewMeasurePage = () => {
           </div>
           <div className="h-[1px] w-full rounded-lg bg-gray-200"></div>
 
-          <div className="grid grid-cols-1 items-stretch gap-4 sm:grid-cols-2  sm:flex-row">
+          <div className="grid grid-cols-1 items-stretch gap-4 sm:grid-cols-3  sm:flex-row">
             {formFieldNames.map(({ name, displayName }) => {
               return (
                 <Input
--- src/server/api/routers/measure.ts
+++ src/server/api/routers/measure.ts
@@ -1,17 +1,56 @@
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import { z } from "zod";
-
 import { createTRPCRouter, protectedProcedure } from "../trpc";
+import type { FormFieldNames } from "../../../pages/measures/new";
+import { CreateMeasure, formFieldNames } from "../../../pages/measures/new";
 
 export const measureRouter = createTRPCRouter({
   create: protectedProcedure
-    .input(z.object({ text: z.string() }))
-    .mutation(async ({ ctx, input }) => {
-      return await ctx.prisma.measure.create({
-        data: {
-          MeasureItem: {
-            createMany: { data: { name: "Łdyka", value: "123" } },
+    .input(CreateMeasure)
+    .mutation(async ({ ctx, input: { date, ...input } }) => {
+      console.log(input);
+      try {
+        const isMeasureField = (
+          fieldName: string
+        ): fieldName is FormFieldNames => {
+          const keys = formFieldNames.map(({ name }) => name);
+          return keys.includes(fieldName as FormFieldNames);
+        };
+
+        const fieldsNameAndId = await ctx.prisma.measureField.findMany({
+          where: { name: { in: Object.keys(input) } },
+          select: { id: true, name: true },
+        });
+
+        const fieldsWithIdAndValue = fieldsNameAndId.reduce<
+          { measureFieldId: string; value: number }[]
+        >((acc, { id, name }) => {
+          if (isMeasureField(name)) {
+            return [
+              ...acc,
+              {
+                measureFieldId: id,
+                value: Number(input[name]),
+              },
+            ];
+          }
+          return acc;
+        }, []);
+
+        return await ctx.prisma.measure.create({
+          data: {
+            User: { connect: { id: ctx.session.user.id } },
+            createdAt: date,
+            MeasureItem: {
+              createMany: {
+                data: fieldsWithIdAndValue,
+              },
+            },
           },
-        },
-      });
+        });
+      } catch (error) {
+        console.log(error);
+        return { error: error };
+      }
     }),
 });
